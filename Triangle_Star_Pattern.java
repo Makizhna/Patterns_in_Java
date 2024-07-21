62c5da709d8d1b70be932cdcc2c26b002d379cd3
Makizhna <134484163+Makizhna@users.noreply.github.com>
2024-07-21 19:17:13 +0530

    Create Triangle_Star_Pattern.java

---
+++ Triangle_Star_Pattern.java
@@ -0,0 +1,24 @@
+CODE:
+class HelloWorld {
+    public static void main(String[] args) {
+        int n=6;
+        for(int i=0;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+OUTPUT:
+       
+      * 
+     * * 
+    * * * 
+   * * * * 
+  * * * * * 
+ * * * * * * 
